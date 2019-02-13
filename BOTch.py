2b662e1b7dac520ddcb0e28f146dd55202f98c5a
crewsz96 <crewsz96@vt.edu>
2019-02-13 12:13:30 -0500

    Added Uptime, UrbanDictionary, and small fixes.

--- BOTch.py
+++ BOTch.py
@@ -5,6 +5,7 @@ from discord import Game
 from discord.ext import commands
 import random
 import asyncio
+import datetime, time
 
 with open('config.json') as f:
     data = json.load(f)
@@ -13,6 +14,7 @@ TOKEN = data["token"]
 BOT_PREFIX = data["prefix"]
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
+start_time = time.time()
 
 #---------------------------------------------#
 @bot.event
@@ -24,6 +26,8 @@ async def on_ready():
     bot.loop.create_task(status_task())
 
 #----------------------------------------------#
+# Task sets the bots "Playing" status to a random string from a list stored
+# in the config.json file.
 async def status_task():
 
     status_messages = data["status_messages"]
@@ -35,7 +39,7 @@ async def status_task():
         await asyncio.sleep(180)
 
 #----------------------------------------------#
-
+# Kill command for the bot.
 @bot.command(name='stop',
             pass_context=True)
 async def stop(context):
@@ -46,7 +50,7 @@ async def stop(context):
     else:
         return
 #---------------------------------------------#
-
+# Roll dice command, follows common dice formatting.
 @bot.command(name='roll',
             description='Rolls given die/dice in the format nDN with the valid die as d2,3,4,6,8,10,12,20,100,1000',
             brief='Rolls a given die/dice',
@@ -72,7 +76,7 @@ async def roll(context):
         return
 
     rolled = 0
-    for i in range(1, num + 1):
+    for i in range(1, num_dice + 1):
         rolled += random.randint(1, die)
 
     if num_dice == 1 and die == 20:
@@ -86,7 +90,7 @@ async def roll(context):
     await bot.say("Your **{0}** rolled a(n) **{1}**.".format(entered_message, rolled))
 
 #-------------------------------------------------------------#
-
+# Pulls a random dog pic from the Dog CEO api.
 @bot.command(name='puppy',
             description='',
             brief='',
@@ -99,5 +103,30 @@ async def puppy():
     image = response.json()["message"]
     await bot.say(image)
 #--------------------------------------------------------------#
+
+@bot.command(name="ud",
+            description='Defines a given term using Urban Dictionary',
+            brief='Urban definition',
+            aliases=['urban', 'UD'])
+async def ud(message):
+
+    url = 'http://api.urbandictionary.com/v0/define?term='
+    url += "{" + message + "}"
+    response = requests.get(url)
+    data = response.json()
+    await bot.say(data["list"][0]["definition"])
+
+#-------------------------------------------------------------#
+
+@bot.command(name='uptime',
+            description='Gives the current uptime of the bot as Hour/Min/Sec',
+            brief='Curent uptime of BOTch',
+            aliases=['up', 'Uptime', 'Up'])
+async def uptime():
+    current_time = time.time()
+    uptime = int(round(current_time - start_time))
+    await bot.say("uptime: {}".format(str(datetime.timedelta(seconds=uptime))))
+
+#-------------------------------------------------------------#
 
 bot.run(TOKEN)
